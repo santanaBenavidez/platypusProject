0d55b221b7708eec3efe60faaa021ee8661cb08d
Maddie <100883964+madisonbartelmuraski@users.noreply.github.com>
2022-03-08 15:01:09 -0800

    Add files via upload

---
+++ platypus.cpp
@@ -0,0 +1,28 @@
+//Implementation file
+#include "platypus.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+Platypus()
+{
+    weight = 0;
+    age = 0;
+    name = "NULL";
+    gender = 'N';
+    status = false;
+    mutant = false;
+}
+
+Platypus(float lbs, short yrs, string nam, char gen)
+{
+    weight = lbs;
+    age = yrs;
+    name = nam;
+    gender = gen;
+    status = true;
+    mutant = false;
+}
