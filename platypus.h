ff62560daf8814b16387fe5338af4db6f1366b74
Maddie <100883964+madisonbartelmuraski@users.noreply.github.com>
2022-03-15 15:37:38 -0700

    Update platypus.h

--- platypus.h
+++ platypus.h
@@ -1,3 +1,11 @@
+/*
+ Filename: platypus.h
+ Summary: This is a program where the user should be able to set a platypus and use certain class member functions
+ to manipulate its values.
+ Authors: Madison Bartel-Muraski, Santana Benavidez, Jonathan Hawes
+ Date: 3/15/2022
+*/
+
 #ifndef PLATYPUS_H
 #define PLATYPUS_H
 #include <iostream>
@@ -13,12 +21,12 @@ public:
     //mutators
     void setWeight(float lbs) { weight = lbs; } //inline function that sets weight to lbs
     void setAge(short yrs) { age = yrs; } //inline function that sets age to yrs
-    void setString(string nam) { name = nam; } //inline function that sets name to nam 
+    void setName(string nam) { name = nam; } //inline function that sets name to nam
     void setGender(char gen) { gender = gen; } //inline function that sets gender to gen
     void setStatus(bool alive) { status = alive; } //inline function that sets status to alive
     void setMutant(bool mut) { mutant = mut; } //inline function that sets mutant to mut
 
-    float getWeight() { return weight; } //Accessor function that return weight 
+    float getWeight() { return weight; } //Accessor function that return weight
     short getAge() { return age; }//Accessor function that returns age
     string getName() { return name; }//Accessor function that returns name
     char getGender() { return gender; }//Accessor function that returns gender
@@ -35,6 +43,7 @@ public:
     char generateGender();//randomly generates gender with a 50% chance of male or female
     float generateWeight();//randomly generates weight within a range of 0.1 to 1.0 pounds
     string generateName();//randomly selects a name according the result of generateGender from list of names
+    void ageMe(); //ages a platypus and and randomly determines if the platypus dies or mutates
 
 private:
     float weight;
@@ -43,8 +52,6 @@ private:
     char gender;
     bool status; //is the platypus alive or dead?
     bool mutant;
-
-    void ageMe(short age, float weight, bool mutant);
 
 };
 
